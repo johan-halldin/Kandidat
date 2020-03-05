3045045e251dc5c07bbcdb5b2bd96017a02d7176
Kalle Åström <kalle@maths.lth.se>
2020-03-05 23:12:28 +0100

    Update dimensionalitetsreduktion_3_testa_autoencoder.m

--- dimensionalitetsreduktion_3_testa_autoencoder.m
+++ dimensionalitetsreduktion_3_testa_autoencoder.m
@@ -6,19 +6,20 @@
 
 %% Define deep learning architecture
 % here layers 2-8 are the encoding and layers 9-13 are the decoding
+KK = 3;
 
 layers = [
     imageInputLayer([50 150 1])
-    convolution2dLayer([3 3],10,'Padding',0)
+    convolution2dLayer([3 3],6,'Padding',0)
     reluLayer
     maxPooling2dLayer(3,'Stride',2)
-    convolution2dLayer([3 3],10,'Padding',0)
+    convolution2dLayer([3 3],6,'Padding',0)
     reluLayer
     maxPooling2dLayer(3,'Stride',2)
-    convolution2dLayer([10 35],3,'Padding',0)
-    transposedConv2dLayer([10 35],10,'Stride',1,'Cropping',0)
+    convolution2dLayer([10 35],KK,'Padding',0)
+    transposedConv2dLayer([10 35],6,'Stride',1,'Cropping',0)
     reluLayer
-    transposedConv2dLayer([6 6],40,'Stride',2,'Cropping',0)
+    transposedConv2dLayer([6 6],6,'Stride',2,'Cropping',0)
     reluLayer
     transposedConv2dLayer([4 4],1,'Stride',2,'Cropping',0)
     regressionLayer];
@@ -35,10 +36,10 @@ options = trainingOptions( 'adam',...
     'Plots', 'training-progress', ...
     'ValidationData',{pos,pos});
 
-if 0, 
+if 1, 
     %Träna denna gång och spara nät
     net = trainNetwork(pos_smooth, pos_smooth, layers, options);
-    save trained_auto_net00004
+    %save trained_auto_net00004
 else
     %Träna inte denna gång utan använd ett nät som är sparat
     load trained_auto_net00003
@@ -54,15 +55,15 @@ blubb = predict(encoder,pos(:,:,:,1))
 
 % decoder
 tmp = saveobj(net);
-tmp.Layers = [imageInputLayer([1 1 3],'Normalization','none');tmp.Layers([9:14])];
+tmp.Layers = [imageInputLayer([1 1 KK],'Normalization','none');tmp.Layers([9:14])];
 decoder = SeriesNetwork.loadobj(tmp)
-blubb2 = predict(decoder,reshape(blubb(1,:),[1 1 3]))
+blubb2 = predict(decoder,reshape(blubb(1,:),[1 1 KK]))
 
 %%
 
 code = predict(encoder,pos);
 
-pos_reconstruct = predict(decoder,reshape(code',1,1,3,size(code,1)));
+pos_reconstruct = predict(decoder,reshape(code',1,1,KK,size(code,1)));
 
 
 %% Visualize code for each image
@@ -96,20 +97,21 @@ end
 %%
 mm = mean(code);
 ss = std(code);
-ee = eye(3);
+ee = eye(KK);
 figure(11);
-for k = 1:3;
+for k = 1:KK;
     subplot(1,2,1);
     colormap(gray);
-    meanimage = predict(decoder,reshape(mm,[1 1 3]));
+    meanimage = predict(decoder,reshape(mm,[1 1 KK]));
     imagesc(meanimage);
     for th = linspace(0,2*pi,100);
         subplot(1,2,2);
         colormap(gray);
-        meanimage = predict(decoder,reshape(mm+cos(th)*ss.*ee(k,:),[1 1 3]));
+        meanimage = predict(decoder,reshape(mm+cos(th)*ss.*ee(k,:),[1 1 KK]));
         imagesc(meanimage);
         title(['Mode: ' num2str(k) ' of ' num2str(3)]);
         pause(0.05);
     end;
-    pause(1);
+    pause;
 end;
+
