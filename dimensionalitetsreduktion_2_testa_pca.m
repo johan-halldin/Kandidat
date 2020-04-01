cf10fb34fba4adaff3b3270c7b92883eb976a044
Johan531 <61194703+Johan531@users.noreply.github.com>
2020-04-01 16:04:21 +0200

    plotta i färg 3d

--- dimensionalitetsreduktion_2_testa_pca.m
+++ dimensionalitetsreduktion_2_testa_pca.m
@@ -34,7 +34,7 @@ for k = 1:min(KK,15);
     imagesc(reshape(model.u(:,k),50,150));
 end
 
-%% Plottar och färgkodar efter klass
+%% Plottar och f?rgkodar efter klass
 
 figure(7);
 hold on
@@ -42,28 +42,51 @@ for dot = 1:length(index)
 
 switch index(2,dot) %letar upp klass i index
     case 0
-        plot(code(dot,1), code(dot,2),'+g') %Normalt värde
+        plot(code(dot,1), code(dot,2),'+g'); %Normalt v?rde
     case 1
         plot(code(dot,1),code(dot,2),'+b');
     case 2
-        plot(code(dot,1), code(dot,2),'+y')
+        plot(code(dot,1), code(dot,2),'+y');
     case 3
-        plot(code(dot,1), code(dot,2),'+r')
+        plot(code(dot,1), code(dot,2),'+r');
 
 %text(code(:,1),code(:,2), string(index));
 end
 end
 
 title('Using two dimensions');
+%% Plottar och f?rgkodar efter klass i tre dimensioner
+
+figure(8);
+
+hold all
+for dot = 1:length(index)
+
+switch index(2,dot) %letar upp klass i index
+    case 0
+        plot3(code(dot,1), code(dot,2), code(dot,3),'+g'); %Normalt v?rde
+    case 1
+        plot3(code(dot,1),code(dot,2), code(dot,3),'+b');
+    case 2
+        plot3(code(dot,1), code(dot,2) ,code(dot,3),'+y');
+    case 3
+        plot3(code(dot,1), code(dot,2), code(dot,3),'+r');
+
+%text(code(:,1),code(:,2), string(index));
+end
+end
+
+title('Using two dimensions');
+hold off
 
 %% Visualize code for each image
 
-figure(7);
+figure(9);
 plot(code(:,1),code(:,2),'*r');
 %text(code(:,1),code(:,2), string(index));
 title('Using two dimensions');
 
-figure(8);
+figure(10);
 hold off;
 plot3(code(:,1),code(:,2),code(:,3),'b*');
 %text(code(:,1),code(:,2), code(:,3), string(index));
